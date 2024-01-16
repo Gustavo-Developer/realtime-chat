b70fe0bd0973a9e73a08af100eb6fc528f9bc2dd
Gustavo <srto.mendes@gmail.com>
2024-01-16 01:45:21 -0300

    Add Cors to app

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ const io = require("socket.io")(server, {
 
 const PORT = 3001;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 io.on("connection", (socket) => {
   console.log("Usuário conectado", socket.id);
 
