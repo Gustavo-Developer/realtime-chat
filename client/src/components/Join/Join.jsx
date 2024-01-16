6593f33dcb6446a0d2cc69b128c1a821d85a5475
Gustavo <srto.mendes@gmail.com>
2024-01-16 01:41:36 -0300

    Update API URL

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -10,7 +10,7 @@ export default function Join({ setChatVisibility, setSocket }) {
   const handleSubmit = async () => {
     const username = usernameRef.current.value;
     if (!username.trim()) return;
-    const socket = await io.connect("http://localhost:3001");
+    const socket = await io.connect("https://realtime-chat-1hhx.vercel.app/");
     socket.emit("set_username", username);
     setSocket(socket);
     setChatVisibility(true);
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require("express");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://realtime-chat-drab.vercel.app/",
   },
 });
 
